7b02e8500ae498efbfc4dbed3c4a0b2b4c8768ff
vander makadi <vanotis720@gmail.com>
2024-02-21 18:04:30 +0200

    done with roboto slab font

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto-slab': ['"Roboto Slab"', 'serif'],
+        'roboto': ['"Roboto"', 'sans-serif'],
+      }
+    },
+    content: [],
+  },
+  plugins: [],
+}
